7aae59b6f893550a8eaac990f7321b9910141ee9
Theng Hongsour <thenghongsour@gmail.com>
2024-10-11 21:29:54 +0700

    create sort order by name or email (use server component)

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,49 +1,38 @@
-"use client";
 import React from "react";
 import { sort } from "fast-sort";
 import Link from "next/link";
-import { useState } from "react";
 
 interface Users {
   id: number;
   name: string;
   email: string;
 }
-
 interface Props {
-  users: Users[];
+  sortOrder: string;
 }
-
-const UserTable = ({ users }: Props) => {
-  const [user, setUser] = useState(users);
-  const sortedName = sort(user).asc((u) => u.name);
-  const sortedEmail = sort(user).asc((u) => u.email);
-
+const UserTable = async ({ sortOrder }: Props) => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 2 },
+  });
+  const users: Users[] = await res.json();
+  const sortedUsers = sort(users).asc(
+    sortOrder === "email" ? (u) => u.email : (u) => u.name
+  );
   return (
     <>
       <table className="table table-zebra">
         <thead>
           <tr>
-            <th
-              onClick={() => {
-                setUser(sortedName);
-              }}
-              className="cursor-pointer"
-            >
+            <th className="cursor-pointer">
               <Link href="/users?sortOrder=name">Name</Link>
             </th>
-            <th
-              onClick={() => {
-                setUser(sortedEmail);
-              }}
-              className="cursor-pointer"
-            >
+            <th className="cursor-pointer">
               <Link href="/users?sortOrder=email">Email</Link>
             </th>
           </tr>
         </thead>
         <tbody>
-          {user.map((user) => (
+          {sortedUsers.map((user) => (
             <tr key={user.id}>
               <td> {user.name}</td>
               <td> {user.email}</td>
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,23 +3,15 @@ import UserTable from "./UserTable";
 interface Props {
   searchParams: { sortOrder: string };
 }
-interface Users {
-  id: number;
-  name: string;
-  email: string;
-}
-const UserPage = async ({ searchParams: { sortOrder } }: Props) => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    next: { revalidate: 2 },
-  });
-  const users: Users[] = await res.json();
+
+const UserPage = ({ searchParams: { sortOrder } }: Props) => {
   return (
     <>
       <h1>User</h1>
       <h2 className="text-blue-600">
         sorted by: <span className="text-green-600"> {sortOrder}</span>
       </h2>
-      <UserTable users={users} />
+      <UserTable sortOrder={sortOrder} />
       <Link href="/" className="btn btn-secondary mt-5 px-5">
         Home
       </Link>
