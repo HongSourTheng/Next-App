78d5d9de1cecc9a7ac3419b746ad360ec814b4d9
Theng Hongsour <thenghongsour@gmail.com>
2024-10-20 15:57:03 +0700

    get user by id and creating user

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET(request: NextRequest) {
@@ -6,4 +7,11 @@ export function GET(request: NextRequest) {
     { id: 2, name: "Jane Doe" },
     { id: 3, name: "Alice Doe" },
   ]);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json(); // // Converts the request body to JSON // parsing
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required." }, { status: 400 });
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 }); // sending
 }
