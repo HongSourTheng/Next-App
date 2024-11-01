2da84869ca4e590820838275d1796e3a54d056a6
Theng Hongsour <thenghongsour@gmail.com>
2024-11-01 15:09:30 +0700

    Lazy Loading use import dynamic from next js and practise library lodash for handle data manipulation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
@@ -5,25 +6,55 @@ import { authOptions } from "./api/auth/[...nextauth]/route";
 import Image from "next/image";
 import room from "@/public/images/room.jpg";
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
-  return (
-    <main className="relative h-screen">
-      {/* <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users" className="text-red-500 pr-2">
-        Users
-      </Link>
-      <Link href="/users/new">NewUser</Link>
+// import HeavyComponent from "./components/HeavyComponent";
 
-      <ProductCard /> */}
-      <Image
-        src={room}
-        alt="room"
-        fill
-        quality={80}
-        sizes="(max-width:480px) 100vw, (max-width:768px) 50vw,33vw"
-        className="object-cover"
-      />
+//use when you want to display on browser when only needed
+import dynamic from "next/dynamic";
+const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+  ssr: false, // when render only on the client side
+  loading: () => <p>Loading...</p>,
+});
+import { useState } from "react";
+
+// import _ from "lodash";
+
+export default function Home() {
+  // const session = await getServerSession(authOptions);
+  const [isVisible, setVisible] = useState(false);
+  return (
+    <main>
+      <h1>Hello World.</h1>
+      <button
+        onClick={async () => {
+          const _ = (await import("lodash")).default;
+          const users = [{ name: "Dom" }, { name: "Mick" }, { name: "Joey" }];
+          const sorted = _.orderBy(users, ["name"]);
+          console.log(sorted);
+        }}
+        className="btn"
+      >
+        Show
+      </button>
+      {/* {isVisible && <HeavyComponent />} */}
     </main>
+    // <main className="relative h-screen">
+    //   <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+    //   <Link href="/users" className="text-red-500 pr-2 font-redHatText_Bold">
+    //     Users
+    //   </Link>
+    //   <Link href="/users/new">NewUser</Link>
+
+    //   <ProductCard />
+
+    //   using Image from Next js
+    //   <Image
+    //     src={room}
+    //     alt="room"
+    //     fill
+    //     quality={80}
+    //     sizes="(max-width:480px) 100vw, (max-width:768px) 50vw,33vw"
+    //     className="object-cover"
+    //   />
+    // </main>
   );
 }
